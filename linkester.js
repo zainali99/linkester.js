79fe36f6d6555a53be7ea2e9e1b06f8dbd3b265b
Zain Ali <zainali99@users.noreply.github.com>
2015-03-09 15:19:44 +0100

    Update linkester.js

--- linkester.js
+++ linkester.js
@@ -1,23 +1,21 @@
 // Linkester.js (C) 2015 by Zain Ali, jsfiler.altervista.org
 window.onload = function() {
 	var obj = document.querySelectorAll("a"),
-		txt1;
+		txt, newTrigger, browser,
+		cacheToken, element,
+		o;
 	for(var i=0; i < obj.length; i++){
-		//add token
 		obj[i].token = "token:"+i;
-		//cache token and obj
-		var cacheToken = obj[i].token,
-			o = obj[i],
-			element = document.createElement("span");
-			getText();
-			element.innerHTML = txt1;
-
-			o.appendChild(element);
-			
+		cacheToken= obj[i].token;
+		o = obj[i];
+		element = document.createElement("span");
+		getText();
+		element.innerHTML = txt;
+		o.appendChild(element);
 			//log
 			console.log(cacheToken);
 			getHttps();
-		
+			edit();
 
 
 	}
@@ -30,13 +28,22 @@ window.onload = function() {
 
 	function getText() {
 		if(o.protocol =="https:"){
-			txt1 = "<div id='protocol-secure'>"+o.protocol+"</div>"+"<div id='hostname'>"+o.hostname+"</div>"+"<div id='pathname'>"+o.pathname+"</div>"+"<div id='title'></div>";
+			txt = "<div id='protocol-secure'>"+o.protocol+"</div>"+"<div id='hostname'>"+o.hostname+"</div>"+"<div id='pathname'>"+o.pathname+"</div>"+"<div id='title'></div>";
 		}
 
 		else {
-			txt1 = "<div id='protocol'>"+o.protocol+"</div>"+"<div id='hostname'>"+o.hostname+"</div>"+"<div id='pathname'>"+o.pathname+"</div>"+"<div id='title'></div>";
+			txt = "<div id='protocol'>"+o.protocol+"</div>"+"<div id='hostname'>"+o.hostname+"</div>"+"<div id='pathname'>"+o.pathname+"</div>"+"<div id='title'></div>";
 		}
 	}
+
+
+function edit(){
+	element.onclick = function(e){
+		
+		e.preventDefault();
+
+	}
+}
 
 
 
