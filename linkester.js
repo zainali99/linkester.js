33124af4d994f657603df52edd33e0a06cd4b6ee
Zain Ali <zainali99@users.noreply.github.com>
2015-11-10 13:55:20 +0100

    Welcome to version 0.2
    changed the event starter, improved the 0.1 version.

--- linkester.js
+++ linkester.js
@@ -1,10 +1,55 @@
-// Linkester.js (C) 2015 by Zain Ali, jsfiler.altervista.org
-window.l = function() {
+// Linkester.js 0.2 (C) 2015 by Zain Ali, http://zainali99.github.io
 
-	var obj = document.querySelectorAll("a"),
-		txt, newTrigger, browser,
-		cacheToken, element,
-		o;
+var linkester = (function() {
+var txt, 
+		  cacheToken, element,
+		  o;
+	
+
+	function  getHttps() {
+		if(o.protocol=="https:"){
+			
+			console.log("token_SECURE_href: "+o)
+		}
+		
+	}
+
+	function getText() {
+    var linkStart = "<div id='linkester-js-element'>";
+    var pStart = "<span id='protocol'>"+o.protocol;
+    var psStart = "<span id='protocol-secure'>"+o.protocol;
+    var hnStart = "<span id='hostname'>//"+o.hostname;
+    var pnStart = "<span id='pathname'>"+o.pathname;
+    var pmStart = "<span id='protocol-mailto'>"+o.protocol;
+    var ftpStart = "<span id='protocol-ftp'>"+o.protocol;
+    var closeDiv = "</div>";
+    var closeSpan = "</span>";
+    
+    
+		if(o.protocol =="https:"){
+			txt = linkStart + psStart + closeSpan + hnStart + closeSpan + pnStart + closeSpan + closeDiv;
+		}
+        else if(o.protocol =="mailto:"){
+        	txt = linkStart + pmStart + closeSpan + hnStart + closeSpan + pnStart + closeSpan + closeDiv;
+        }
+        else if(o.protocol =="ftp:"){
+        	txt = linkStart + ftpStart + closeSpan + hnStart + closeSpan + pnStart + closeSpan + closeDiv;
+        }
+        
+        
+
+		else {
+			txt = linkStart + pStart + closeSpan + hnStart + closeSpan + pnStart + closeSpan + closeDiv;
+		}
+	}
+  
+  
+  return {
+    applyTo: function(element) {
+      
+      //0.2
+      var where = document.querySelector(element);
+      var obj = where.querySelectorAll("a");
         
 	for(var i=0; i < obj.length; i++){
 		obj[i].token = "token:"+i+"_href:"+obj[i];
@@ -21,37 +66,19 @@ o.insertAdjacentHTML ("afterEnd", txt);
 
 
 	}
-
-	function  getHttps() {
-		if(o.protocol=="https:"){
-			
-			console.log("token_SECURE_href: "+o)
-		}
-		
-	}
-
-	function getText() {
-		if(o.protocol =="https:"){
-			txt = "<div id='linkester-js-element'><span id='protocol-secure'>"+o.protocol+"</span>"+"<span id='hostname'>//"+o.hostname+"</span>"+"<span id='pathname'>"+o.pathname+"</span>"+"<span id='title'></span></span>";
-		}
-        else if(o.protocol =="mailto:"){
-        	txt = "<div id='linkester-js-element'><span id='protocol-mailto'>"+o.protocol+"</span>"+"<span id='hostname'>//"+o.hostname+"</span>"+"<span id='pathname'>"+o.pathname+"</span>"+"<span id='title'></span></span>";
-        }
-        else if(o.protocol =="ftp:"){
-        	txt = "<div id='linkester-js-element'><span id='protocol-ftp'>"+o.protocol+"</span>"+"<span id='hostname'>//"+o.hostname+"</span>"+"<span id='pathname'>"+o.pathname+"</span>"+"<span id='title'></span></span>";
-        }
-        
-        
-
-		else {
-			txt = "<div id='linkester-js-element'><span id='protocol'>"+o.protocol+"</span>"+"<span id='hostname'>//"+o.hostname+"</span>"+"<span id='pathname'>"+o.pathname+"</span>"+"<span id='title'></span></span>";
-		}
-	}
+    },
+  
+    
+    getSecureConnections: function() {
+      if(o.protocol=="https:"){
+			console.log("token_SECURE_href: "+o);
+		  }
+    }
+    
+  }
+    
+  
+})();
 
 
-
-
-}
-/*
-l(); event-starter.
-*/
+ 
